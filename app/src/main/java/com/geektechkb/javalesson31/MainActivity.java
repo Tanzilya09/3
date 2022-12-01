7dba19f8eefd5ab06963286cf2ac91d4bd97997c
Танзиля09 <muhamedjanova20@gmail.com>
2022-12-01 16:31:26 +0600

    Initial commit

---
+++ app/src/main/java/com/geektechkb/javalesson31/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geektechkb.javalesson31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
